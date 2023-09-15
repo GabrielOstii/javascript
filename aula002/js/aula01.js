195f0ab4ecbdde3b5e2f055f688cae192662b746
Gabriel Carlos Osti Silva <112735985+GabrielOstii@users.noreply.github.com>
2023-09-15 17:46:46 -0400

    Update a1 ate a3

---
+++ aula002/js/aula01.js
@@ -0,0 +1,39 @@
+document.write("Ola Mundo!");
+console.log("Outra olá mundo!");
+/*
+bloco
+*/
+//linha
+
+// let 'Nome' = Conteudo
+let mensagem = "Minha mensagem";
+// Siga um padrão
+let meuPeso = 70.5;
+let meu_peso = 70;
+
+let minhaAltura = 1.75;
+let minhaIdade = 19;
+
+let ehDoador = false;
+
+let teste = undefined // VAZIO "", '', null, undefined, 0, 0.0,
+
+// Constantes
+const PI = 3.1415;
+const TAXA = 0.5;
+
+teste = "Algum conteúdo"
+
+// Escrever o conteudo de variaveis e constantes
+document.write(mensagem)
+document.write(meuPeso)
+document.write(meu_peso)
+document.write(minhaAltura)
+document.write(minhaIdade)
+document.write(ehDoador)
+document.write(teste)
+document.write(PI)
+document.write(TAXA)
+
+document.write("<p> Mensagem " + mensagem + "<p>");
+document.write(`Altura: ${minhaAltura}m`)
---
+++ aula004/aula004.js
@@ -0,0 +1,20 @@
+let podutos = ['Arroz', 'Feijão', 'Leite'];
+var codigos = Array(10, 20, 30);
+let meses = ['Jan', 'Fev', 'Mar', 'Abr'];
+
+// Adicionar no final push = empurre
+produtos.push('Açúcar');
+codigos.push(40, 50, 60, 70);
+meses.push('Mai', 'Jun','Ago', '07')
+
+//Remover do final pop = estourar
+produtos.pop();
+codigos.pop();
+meses.pop();
+
+// Adicionar no inicio unshift
+produtos.unshift('Uva', 'Maçã');
+
+//Remover no inicio shift
+produtos.shift();
+
