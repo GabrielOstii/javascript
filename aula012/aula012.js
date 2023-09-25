52def9f83ff12ce76c0c5ea5825893156ceb247b
Gabriel Carlos Osti Silva <112735985+GabrielOstii@users.noreply.github.com>
2023-09-25 08:42:34 -0400

    Update

---
+++ aula012/aula012.js
@@ -0,0 +1,25 @@
+let sit1 = document.querySelector('#sit1')
+let sit2 = document.querySelector('#sit2')
+let sit3 = document.querySelector('#sit3')
+
+let notaFinal1 = 3
+let notaFinal2 = 8
+let notaFinal3 = 5
+
+/*Condicionais*/
+
+/*IF*/
+if (notaFinal1 >= 7) {
+    sit1.textContent = 'Aprovado'
+}
+
+/*IF ELSE*/
+
+if (notaFinal2 >=7 ) {
+    sit2.textContent = 'Aprovado(a)'
+} else {
+    sit2.textContent = 'Reprovado(a)'
+}
+
+/*IF TERNARIO*/
+
