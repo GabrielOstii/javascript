f33ad096694280b5cd085b8f926c860dda2340a6
Gabriel Carlos Osti Silva <112735985+GabrielOstii@users.noreply.github.com>
2023-12-13 14:38:53 -0400

    Update relogiosmart.js

--- projetos/relogio-digital/relogiosmart.js
+++ projetos/relogio-digital/relogiosmart.js
@@ -82,3 +82,52 @@ function pegarData() {
 }
 
 pegarData()
+
+var options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+}
+
+let teste = new Date()
+
+console.log(teste.toLocaleString('pt-BR'))
+console.log(teste.toLocaleString('pt-BR', options))
+console.log(teste.toLocaleDateString('pt-BR'))
+console.log(teste.toLocaleTimeString('pt-BR'))
+
+function getUserPosition() {
+    let url = ''
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let lat = pos.coords.latitude
+      let long = pos.coords.longitude
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=6579ce2073ca8a90ddce06187a619070`
+      fetchApi(url)
+      console.log(url)
+    })
+  }
+  
+  function fetchApi(url) {
+    let city = document.querySelector('.city')
+    let temperature = document.querySelector('#temp')
+    let humidity = document.querySelector('#umidad')
+  
+    fetch(url)
+    .then((data) => {
+      return data.json()
+    })
+    .then((data) => {
+      let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
+      
+      city.textContent      = data.name
+      temperature.innerHTML = tempInCelsius
+      humidity.innerHTML    = data.main.humidity
+    })
+    .catch((err) => {
+      city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
+      temperature.innerHTML = `-`;
+    })
+  }
+  
+  getUserPosition();
