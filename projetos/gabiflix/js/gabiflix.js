9971ada040def7438674b89ad0ff68fb25c9b686
Gabriel Carlos Osti Silva <112735985+GabrielOstii@users.noreply.github.com>
2024-02-16 09:09:30 -0400

    Update

--- projetos/gabiflix/js/gabiflix.js
+++ projetos/gabiflix/js/gabiflix.js
@@ -2,19 +2,38 @@
 let ulsElis = document.querySelectorAll('ul li')
 //console.log(ulsElis)
 
-let qtdItens = ulsElis.length
-console.log('Quant. lis = ' + qtdItens)
+//cada lista é uma categoria
+let numDeListas = document.querySelectorAll('ul').length
 
-let numDeItens = 6
+
+//quant total de lis de todas as listas
+let qtdVideos = ulsElis.length
+console.log('Quant. Videos = ' + qtdVideos)
+
+let numDeItens = 5 // Capas visiveis no carrosel
 
 //... Criar mais conforme as listas
+// Forma inteligente utilizando Arraw
+let lista = [];
+for(let x = 1; x <= numDeListas; x++){
+    lista[x] = document.querySelectorAll('#lista'+x+' li')
+}
+/*
 let lista1 = document.querySelectorAll('#lista1 li')
 let lista2 = document.querySelectorAll('#lista2 li')
+*/
 
 let numDeVideos = document.querySelectorAll('.numDeVideos')
 
+for(let y = 0; y < numDeListas; y++){
+    numDeVideos[y].textContent = lista[y+1].length
+}
+
+/*
 numDeVideos[0].textContent = lista1.length
 numDeVideos[1].textContent = lista2.length
+*/
+
 
 function show(indice, indicelista) {
     let n = indice
@@ -35,4 +54,11 @@ function show(indice, indicelista) {
     if(indice == -1) {
         listaUl.scrollBy(posicaoXEsquerda, 0)
     }
+}
+
+let iframeVideo = document.querySelector('#iframeVideo')
+
+function abrirModal(videoId){
+    location.href="#abrirModal"
+    iframeVideo.setAttribute('src', `https://www.youtube.com/embed/${videoId}`)
 }
